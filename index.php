c2dcae3508e60d6edc5941b5bf19cea48066f255
sukhdevr898 <59791782+sukhdevr898@users.noreply.github.com>
2021-05-26 17:15:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<html>
+    <head>
+        <script>
+            <l--hide
+var password
+var pass1="sardar ji"
+          password=prompt("enter your password");
+    if("password=pass1");
+       alert("valid password click ok to continue");
+       else
+{
+    window.location("index2.html");
+}
+    
+            
+            
+           
+        </script>
+        <title>
+            LAUNCH BOMBER
+            </title>
+    </head>
+    <body background="ibg.jpg" text="white">
+       <h1 id="h"></h1>
+      <h1> <script>
+       window.alert("If your are misuse this tool we are not responsible follow me on instagam @sukhdevr898 and press ok button")
+       </script>
+       </h1>
+       
+       
+       
+       
+       <marquee bgcolor="red width="70" height="50">WARRING:- we are not responsible if victim's phone has been damege</marquee>
+        <h1 style="text-align: center;"> please don't Misuse this tool only for educational purposes
+
+
+ </h1><hr>
+ <h1>click launch button for use this tool</h1><hr>
+        
+      <h1 style="text-align:center; font-size:10em;"><button style="border-style: dotted; background-color:green; font-size: 1em;"><a href="http://sbomb.000webhostapp.com/index2.php" style="font-size: 0.5em;">
+          LAUNCH
+          </a>        
+        </button></h1><hr>
+        <h2>
+            thanking you 🤗 for using this tool 
+            
+            
+            
+            </h2>
+            <h2>
+                if you want to contact with use 
+                click here 👇🏻👇🏻
+                
+                
+                </h2 style="color: red;">
+                <a href="https://sukhdevr898.site123.me/contact-us">contact us</a>
+                <hr>
+                <h2>
+                sppourt us<br>
+<a href="https://m.facebook.com/sukhdevr898" >FACEBOOK</a><br>
+<a href="https://m.me/sukhdevr898">MESSENGER</a>
+<br>
+<a href="https://m.instagram.com/sukhdevr898">ÏÑSSTÀGRÁM</a>
+
+
+  <br>
+  <a href="https://wa.me/+14848502204" >WHATSAPP</a>
+                
+                
+                
+                
+               </h2>
+    </body>
+    
+    
+    
+    
+    
+</html>
---
+++ text2.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        
+       <title>Sbomb</title>
+       </head>
+       <body bgcolor="black" text="white">
+           
+         <script>
+       window.alert("If your are misuse this tool we are not responsible follow me on instagam @divya.naik898 and press ok button")
+       </script>
+           <hr>
+           <h1>
+               developed by <b>Divya naik
+           </b></h1>
+           <hr>
+           <h1>LAUNCH BOMBER TO CLICK HERE 👉🏻👉🏻👉🏻<a href="https://sbomb.000webhostapp.com/text1.php"
+         >LAUNCH</a>
+           </h1>
+           
+       </body>
+    
+    
+</html>

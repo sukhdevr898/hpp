36f058400a6e84402919aaf8fc812f1e2acf541b
sukhdevr898 <59791782+sukhdevr898@users.noreply.github.com>
2021-06-30 16:13:02 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "hello geek welcome to our tool"
-read -p "ENTER YOUR NAME : " name
-pkg install figlet -y
-figlet $name
-echo "dont misuse other we are not responsable"
-rm install.sh
-bash core/in.sh
-

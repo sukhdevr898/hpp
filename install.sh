78c923da75afb16255f528e8c65337d24e9bb1f0
sukhdevr898 <59791782+sukhdevr898@users.noreply.github.com>
2021-06-30 11:28:22 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "hello geek welcome to our tool"
-read -p "ENTER YOUR NAME : " name
-figlet $name
-echo "dont misuse other we are not responsable"
-rm install.sh
-bash core/in.sh
-

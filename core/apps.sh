d28608f62fe387c050d7441797b150959fb998be
sukhdevr898 <59791782+sukhdevr898@users.noreply.github.com>
2021-07-01 09:42:55 +0530

    Update apps.sh

--- core/apps.sh
+++ core/apps.sh
@@ -21,73 +21,137 @@ echo "8 => back"
  #case
 case "$app" in 
   1)
-echo "PLEASE WAIT..."
+echo -e "\e[1;31m \n\t\t PLEASE WAIT."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT.."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT..."
+sleep 1.0
+clear
 cd $HOME
 cd /sdcard/sukhdevr898
 wget https://download1082.mediafire.com/h5y61ybh43pg/oavzscf85ws47t8/5_6080304459857002813.apk
-acho -e "\n\t\tDOWNLOAD COMPLETED..."
-echo -e "YOUR FILE SAVED IN INTERNAL STORAGE IN SUKHDEVR898 FOLDER"
+echo -e "\e[1;31m \n\t\tDOWNLOAD COMPLETED..."
+echo -e "\e[1;31n \n\t\tYOUR FILE SAVED IN INTERNAL STORAGE IN SUKHDEVR898 FOLDER"
 cd $HOME/sbomb/core
 bash apps.sh
 
 ;;
 2)
-echo "PLEASE WAIT..."
+echo -e "\e[1;31m \n\t\t PLEASE WAIT."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT.."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT..."
+sleep 1.0
+clear
 cd $HOME
 cd /sdcard/sukhdevr898
 wget https://files.moddroid.com/Truecaller/Truecalle_11.65.7.apk
-acho -e "\n\t\tDOWNLOAD COMPLETED..."
-echo -e "YOUR FILE SAVED IN INTERNAL STORAGE IN SUKHDEVR898 FOLDER"
+echo -e "\e[1;31m \n\t\tDOWNLOAD COMPLETED..."
+echo -e "\e[1;31n \n\t\tYOUR FILE SAVED IN INTERNAL STORAGE IN SUKHDEVR898 FOLDER"
 cd $HOME/sbomb/core
 bash apps.sh
 ;;
 
 3)
-echo "PLEASE WAIT..."
+echo -e "\e[1;31m \n\t\t PLEASE WAIT."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT.."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT..."
+sleep 1.0
+clear
 cd $HOME
 cd /sdcard/sukhdevr898
 wget https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=2341039
-acho -e "\n\t\tDOWNLOAD COMPLETED..."
-echo -e "YOUR FILE SAVED IN INTERNAL STORAGE IN SUKHDEVR898 FOLDER"
+echo -e "\e[1;31m \n\t\tDOWNLOAD COMPLETED..."
+echo -e "\e[1;31n \n\t\tYOUR FILE SAVED IN INTERNAL STORAGE IN SUKHDEVR898 FOLDER"
 cd $HOME/sbomb/core
 bash apps.sh
 ;;
 4)
-echo "PLEASE WAIT..."
+echo -e "\e[1;31m \n\t\t PLEASE WAIT."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT.."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT..."
+sleep 1.0
+clear
 cd $HOME
 cd /sdcard/sukhdevr898
 wget https://download2388.mediafire.com/gh101miygkpg/dolaun4833zfpco/com.instagram.android_277249234.apk
-acho -e "\n\t\tDOWNLOAD COMPLETED..."
-echo -e "YOUR FILE SAVED IN INTERNAL STORAGE IN SUKHDEVR898 FOLDER"
+echo -e "\e[1;31m \n\t\tDOWNLOAD COMPLETED..."
+echo -e "\e[1;31n \n\t\tYOUR FILE SAVED IN INTERNAL STORAGE IN SUKHDEVR898 FOLDER"
 cd $HOME/sbomb/core
 bash apps.sh
 ;;
 5)
-echo "PLEASE WAIT..."
+echo -e "\e[1;31m \n\t\t PLEASE WAIT."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT.."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT..."
+sleep 1.0
+clear
 cd $HOME
 cd /sdcard/sukhdevr898
 wget https://download1466.mediafire.com/pixtyuusizkg/eeiq1msrhbjpu2p/com.whatsapp_210422007.apk
-acho -e "\n\t\tDOWNLOAD COMPLETED..."
+echo -e "\e[1;31m \n\t\tDOWNLOAD COMPLETED..."
+echo -e "\e[1;31n \n\t\tYOUR FILE SAVED IN INTERNAL STORAGE IN SUKHDEVR898 FOLDER"
 cd $HOME/sbomb/core
-echo -e "YOUR FILE SAVED IN INTERNAL STORAGE IN SUKHDEVR898 FOLDER"
 bash apps.sh
 ;;
 6)
-echo "PLEASE WAIT..."
+echo -e "\e[1;31m \n\t\t PLEASE WAIT."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT.."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT..."
+sleep 1.0
+clear
 cd $HOME
 cd /sdcard/sukhdevr898
 wget http://download1593.mediafire.com/8adcg2sh40sg/wr7qug6t68ybzso/thoptv_v40.apk
-acho -e "\n\t\tDOWNLOAD COMPLETED..."
+echo -e "\e[1;31m \n\t\tDOWNLOAD COMPLETED..."
+echo -e "\e[1;31n \n\t\tYOUR FILE SAVED IN INTERNAL STORAGE IN SUKHDEVR898 FOLDER"
 cd $HOME/sbomb/core
-echo -e "YOUR FILE SAVED IN INTERNAL STORAGE IN SUKHDEVR898 FOLDER"
 bash apps.sh
 ;;
 7)
-echo "PLEASE WAIT..."
-echo "go to our site LINK :- https://sukhdevr898.site123.me/termux-commands/best-android-apps-list"
+echo -e "\e[1;31m \n\t\t PLEASE WAIT."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT.."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT..."
+sleep 1.0
+clear
+echo -e "\e[102m \n\t go to our site LINK :- https://sukhdevr898.site123.me/termux-commands/best-android-apps-list"
 ;;
 8)
-echo "PLEASE WAIT..."
+echo -e "\e[1;31m \n\t\t PLEASE WAIT."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT.."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT..."
+sleep 1.0
+clear
 cd .. 
 bash sbomb.sh
 ;;

d684c5fe03862645e9510d632f55a3fd7f1570e9
sukhdevr898 <59791782+sukhdevr898@users.noreply.github.com>
2021-07-01 09:45:34 +0530

    Update run.sh

--- core/run.sh
+++ core/run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
   #comma
+echo -e "\e[1;31m \n\t\t PLEASE WAIT."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT.."
+sleep 1.0
+clear
+echo -e "\e[1;31m \n\t\t PLEASE WAIT..."
+sleep 1.0
+clear
 python run.py
 
-echo "$(php -S localhost:8080)"
+echo -e "\e[0;31m \n\t\t$(php -S localhost:8080)"
 
 

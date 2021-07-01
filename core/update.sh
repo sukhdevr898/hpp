c55b87622361dc53b38bb8ece9c5cd212f550e6a
sukhdevr898 <59791782+sukhdevr898@users.noreply.github.com>
2021-07-01 08:56:51 +0530

    Update update.sh

--- core/update.sh
+++ core/update.sh
@@ -1,7 +1,7 @@
 cd
 rm -rf sbomb
 git clone https://github.com/sukhdevr898/sbomb
-cd sbomb
 
-figlet updated
-bash sbomb.sh
+figlet -f big updated
+echo -e "\e[1;31m \n\t\tNEW VERSION 2.3.2 CLRS UPDATED \n\t\tRESTART TERMUX AND OPEN AGAIN "
+

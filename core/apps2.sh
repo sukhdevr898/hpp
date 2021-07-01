532dc8f4684dde8c40d6bc463c3c9195e6cd2534
sukhdevr898 <59791782+sukhdevr898@users.noreply.github.com>
2021-07-01 10:01:47 +0530

    Update apps2.sh

--- core/apps2.sh
+++ core/apps2.sh
@@ -3,7 +3,7 @@
 
 
 
-echo -e "\n\t\tIF YOU CHOOSE THIS OPTION FIRST TIME \nTHEN TYPE Y \nOTHERWISE TYPE N "
+echo -e "\e[101m \n\t\tIF YOU CHOOSE THIS OPTION FIRST TIME \nTHEN TYPE Y \nOTHERWISE TYPE N "
   read -p "ARE YOU CREATE DIR ? : " d
 case "$d" in
 Y)
@@ -11,7 +11,7 @@ cd $HOME
 cd /sdcard
 mkdir sukhdevr898
 cd sukhdevr898
-echo "DIR CREATED"
+echo -e "\e[102m \n\t DIR CREATED"
 cd
 cd sbomb/core
 bash apps.sh

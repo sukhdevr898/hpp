2804f2bc2370c3eca60cd0d9ebfc643bc31a64d5
sukhdevr898 <59791782+sukhdevr898@users.noreply.github.com>
2021-07-01 10:09:36 +0530

    Update best.sh

--- core/best.sh
+++ core/best.sh
@@ -22,20 +22,38 @@ read -p "SELECT A OPTION : " op
 #case     
 case "$op" in
   1)
-  echo "INSTALLING..."
-   echo "$(cd $HOME  && apt-get update -y &&  pkg install python -y && pkg install python2 -y && pkg install git -y && pip install lolcat && git clone https://github.com/noob-hackers/mrphish && cd $HOME && cd mrphish && ls && bash setup && bash mrphish)"
+  echo -e "\e[1;31m INSTALLING..."
+   echo -e "\e[1;32m $(cd $HOME  && apt-get update -y &&  pkg install python -y && pkg install python2 -y && pkg install git -y && pip install lolcat && git clone https://github.com/noob-hackers/mrphish && cd $HOME && cd mrphish && ls && bash setup && bash mrphish)"
   ;;
 2)
- echo "INSTALLING..."
- echo "$(pkg install python -y && pkg install python2 -y && pip install lolcat && cd $HOME  && git clone https://github.com/noob-hackers/spamx && cd spamx && bash setup && bash spamx.sh )"
+ echo -e "\e[1;31m INSTALLING."
+sleep 0.5
+clear
+   echo -e "\e[1;31m INSTALLING.."
+sleep 0.5
+clear
+   echo -e "\e[1;31m INSTALLING..."
+  sleep 0.5
+clear 
+ 
+   
+ echo -e "$(pkg install python -y && pkg install python2 -y && pip install lolcat && cd $HOME  && git clone https://github.com/noob-hackers/spamx && cd spamx && bash setup && bash spamx.sh )"
  ;;
    3)
-echo "INSTALLING..."
-  echo "$(cd $HOME &&  pkg install curl && wget && git clone git://github.com/htr-tech/zphisher.git && cd zphisher && bash zphisher.sh )"
+echo -e "\e[1;31m INSTALLING."
+sleep 0.5
+clear
+   echo -e "\e[1;31m INSTALLING.."
+sleep 0.5
+clear
+   echo -e "\e[1;31m INSTALLING..."
+  sleep 0.5
+clear 
+  echo -e "$(cd $HOME &&  pkg install curl && wget && git clone git://github.com/htr-tech/zphisher.git && cd zphisher && bash zphisher.sh )"
   ;;
    4)
-echo "THANKING YOU....!."
-echo "WAITING..."
+echo -e "THANKING YOU....!."
+echo -e "WAITING..."
 cd ..
 bash sbomb.sh
 
